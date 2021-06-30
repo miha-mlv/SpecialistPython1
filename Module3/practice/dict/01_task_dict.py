d7df03882158cef417ec13ce58bbcb689372d696
miha-mlv <74875181+miha-mlv@users.noreply.github.com>
2021-06-30 16:59:26 +0300

    Update 01_task_dict.py

--- Module3/practice/dict/01_task_dict.py
+++ Module3/practice/dict/01_task_dict.py
@@ -1,10 +1,7 @@
-# Дан словарь, содержащий данные о товаре из магазина
-# "price" - цена товара в валюте "currency"
-# "count" - количествотовара в магазине
-# Считая,что курс доллара равен dollar_rate
-# Вычислите цену всех товаров с названием "name" в долларах
-
 item = {"name": "Кроссовки", "price": "7540.5", "currency": "rub", "count": "10"}
 dollar_rate = 74.12
-
-# TODO: your code here
+a = float(item["price"])
+a1 = dollar_rate * a
+b = int(item["count"])
+c = a1 * b
+print(c)
