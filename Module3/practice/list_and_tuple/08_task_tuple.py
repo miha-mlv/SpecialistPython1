f39597facb44e4f5ee205292c36c13979dcf9c3e
miha-mlv <74875181+miha-mlv@users.noreply.github.com>
2021-06-30 14:31:48 +0300

    Update 08_task_tuple.py

--- Module3/practice/list_and_tuple/08_task_tuple.py
+++ Module3/practice/list_and_tuple/08_task_tuple.py
@@ -1,15 +1,36 @@
-# Даны примеры создания кортежей.
-# Выясните, какие из них являются корректными.
-# Все некорректные объявления и создающие не котрежи закомментируйте (#)
+i = 0
 tup1 = (2, 4, -6)
+print('1' + str(type(tup1)))
+
 tup2 = (2, "Василия", -6)
+print(type(tup2))
+
 tup3 = 2, 4, -6
-tup4 = (2)
+print(type(tup3))
+
+# tup4 = (2)
+# print(type(tup4))
+
 tup5 = (2,)
+print(type(tup5))
+
 tup6 = 2,
+print(type(tup6))
+
 tup7 = tuple([2, 4, -6, 12])
-tup8 = tuple(2, 7, 8, -5)
+print(type(tup7))
+
+# tup8 = tuple(2, 7, 8, -5)
+# print(type(tup8))
+
 tup9 = tuple()
+print(type(tup9))
+
 tup10 = tuple("hello")
+print(type(tup10))
+
 tup11 = ()
-tup12 = 2 and 4
+print(type(tup11))
+
+# tup12 = 2 and 4
+# print(type(tup12))
