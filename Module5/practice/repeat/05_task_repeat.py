b0d7b0334fa36429bd21c3a7754aa8c662240bf9
miha-mlv <74875181+miha-mlv@users.noreply.github.com>
2021-07-02 11:36:30 +0300

    Update 05_task_repeat.py

--- Module5/practice/repeat/05_task_repeat.py
+++ Module5/practice/repeat/05_task_repeat.py
@@ -1,3 +1,14 @@
-# Дано общее количество объектов для отображения на веб-сайте и количество объектов на каждой странице.
-# Вычислите, сколько объектов будет отображаться на последней странице сайта.
+def pagination(num_items, items_on_page):
+    a = num_items / items_on_page
+    b = num_items % items_on_page
+    q = num_items // items_on_page
+    if b == 0:
+        return "На последней странице будет отображено: " + str(items_on_page)
+    else:
+        c = q * items_on_page
+        qw = num_items - c
+        return "На последней странице будет отображено: " + str(qw)
 
+num_items = int(input('num_items = '))
+items_on_page = int(input("items_on_page = "))
+print(pagination(num_items, items_on_page))
